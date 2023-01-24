9cb07c986a6eb75d9ff2edffe4033cab6bd96948
hibaq obsiye <obsiyehibaq@gmail.com>
2023-01-24 19:57:04 +0000

    converting numerical operation to string operation in order to carry out a mathematical operation

---
+++ 8kyu/javascript/basicMathOperation.js
@@ -0,0 +1,9 @@
+//Your task is to create a function that does four basic mathematical operations.
+// The function should take three arguments - operation(string/char), value1(number), value2(number).
+// The function should return result of numbers after applying the chosen operation.
+
+function basicOp(operation, value1, value2)
+{
+ let x = String(value1) + operation + String(value2)
+ return eval(x)
+}
