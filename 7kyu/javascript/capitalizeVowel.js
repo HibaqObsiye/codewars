6b077d18ca3af95fb2db846132f4107ed9badb24
hibaq obsiye <obsiyehibaq@gmail.com>
2023-03-09 22:47:30 +0000

    capitalize the vowels

--- 7kyu/javascript/capitalizeVowel.js
+++ 7kyu/javascript/capitalizeVowel.js
@@ -9,16 +9,13 @@
 // Note: Y is not a vowel in this kata.
 
 function swap (string) {
-  
-    let vowels = ["a", "e", "i", "o", "u"]
-      let modifiedString = ""
-    for(let i = 0; i < vowels.length; i++){
-      if(vowels.includes(string[i])){
-        modifiedString += string[i].toUpperCase()
-      }
-      else{
-       modifiedString += string[i]
-      }     
-    }
-    return modifiedString 
+    let stringSplit = string.split("")
+   let vowels = ["a", "e", "i", "o", "u"]
+ 
+   for(let i = 0; i < stringSplit.length; i++){
+     if(vowels.includes(stringSplit[i])){
+       stringSplit[i] = stringSplit[i].toUpperCase()
+     }
+   }
+   return stringSplit.join("")
 }
